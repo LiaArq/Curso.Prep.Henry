364c2e83fab7cefcaa571b3facb2a79f04ab3517
Lia Abregu <arq.lia.abregu@gmail.com>
2022-01-14 18:31:03 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,37 +28,37 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
 // "Return" la string provista: str
   // Tu código: 
-  return str
+  return str;
 
 }
 
 
-function suma(x, y) {
+function sumar(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  return x+y 
+  return x + y;
 
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x-y
+  return x - y;
 
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  return x*y
+  return x * y;
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return x/y
+  return x / y;
   
 }
 
@@ -66,11 +66,12 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (x===y)
+    if (x === y) {
     return true;
-} else {
-  (x! ==y)
-  return false
+    } else {
+      return false;
+
+    }
 
 }
 
@@ -80,6 +81,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+
   
 }
 
@@ -87,8 +89,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-}
+  if  ( num < 90) ;
+  return true 
+} else
+return false
+
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
@@ -237,7 +242,7 @@ module.exports = {
   sonIguales,
   menosQueNoventa,
   mayorQueCincuenta,
-  suma,
+  sumar,
   resta,
   divide,
   multiplica,
